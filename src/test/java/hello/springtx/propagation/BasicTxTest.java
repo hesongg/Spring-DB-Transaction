28da30b538339625248a7c5afa9a52c21c4f701d
hesung <hsjo6864@gmail.com>
2022-08-29 00:15:20 +0900

    2022.08.28 Spring Transaction Study Project - 트랜잭션 전파 테스트 - 트랜잭션 전파 (REQUIRES_NEW)  - 내부 트랜잭션 전파 옵션으로 PROPAGATION_REQUIRES_NEW 를 사용하는 경우   - 기존 트랜잭션에 참여하지않고 신규 트랜잭션으로 명확히 분리된다

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
@@ -125,6 +126,26 @@ public class BasicTxTest {
 
         Assertions.assertThatThrownBy(() -> transactionManager.commit(outer))
                 .isInstanceOf(UnexpectedRollbackException.class);
+    }
+
+    @Test
+    void inner_rollback_requires_new() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = transactionManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaciton()={}", outer.isNewTransaction()); //true
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+
+        TransactionStatus inner = transactionManager.getTransaction(definition);
+        log.info("inner.isNewTransaciton()={}", inner.isNewTransaction()); //true
+
+        log.info("내부 트랜잭션 롤백");
+        transactionManager.rollback(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        transactionManager.commit(outer);
     }
 
 }
